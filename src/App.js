d52e073e352bbe10f1cf9b66748a59c2dc75736c
tmaa <thomasaanum@gmail.com>
2022-02-06 14:34:52 -0500

    added email

--- src/App.js
+++ src/App.js
@@ -1,16 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import {getAuth, signInAnonymously, signOut, deleteUser} from 'firebase/auth'
+import axios from 'axios';
 
 function App() {
+  const [phoneNumber, setPhoneNumber] = useState()
+  const [registerSuccess, setRegisterSuccess] = useState(false)
+  const [id, setId] = useState()
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [height, setHeight] = useState("")
+  const [email, setEmail] = useState("")
+
+  function onAnonClick(){
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then((res) => {
+        setId(res.user.uid)
+        setRegisterSuccess(true)
+        console.log(res)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + ": " + errorMessage)
+      });  }
+
+  function onPhoneClick(number){
+    console.log(number)
+  }
+
+  function logoutClick(){
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if(user.isAnonymous){
+      deleteUser(user).then(() => {
+        axios.delete(`http://localhost:3001/api/users/delete/${id}`)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        setRegisterSuccess(false)
+        console.log("anon user deleted")
+      }).catch((err) => {
+        console.log(err)
+      })
+    }else{
+      signOut(auth).then(() => {
+        setRegisterSuccess(false)
+        console.log("logged out: ", user)
+      }).catch((error) => {
+        console.log("err: ", error)
+      });
+    }
+  }
+
+  function handleSubmit(e){
+    var data = {"id": id, "name": name, "age": age, "height": height, "email": email}
+    
+    axios.post(`http://localhost:3001/api/users/register`, data)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    // setName("")
+    // setAge("")
+    // setHeight("")
+    // setEmail("")
+    //logoutClick()
+    //setRegisterSuccess(false)
+    e.preventDefault()
+  }
+
+  function getCurrentUserInfo(){
+    //use to test token/auth protected api
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-          Lear
-        </a>
+        <button>log in with Google</button>
+        <div>
+          <button onClick={() => onPhoneClick(phoneNumber)}>log in with phone number</button>
+          <input onChange={e => setPhoneNumber(e.target.value)}></input>
+        </div>
+        <button onClick={() => onAnonClick()}>anonymous sign in</button>
+        <button onClick={() => logoutClick()}>delete/log out</button>
+        <h1>{phoneNumber}</h1>
+        {registerSuccess ? (
+          <div>
+            <form onSubmit={handleSubmit}>
+              <h1>success {id}</h1>
+              <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder='name'></input>
+              <input type="text" value={age} onChange={e => setAge(e.target.value)} placeholder='age'></input>
+              <input type="text" value={height} onChange={e => setHeight(e.target.value)} placeholder='height'></input>
+              <div>
+                <input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder='email'></input>
+              </div>
+              <div><button type="submit">complete registration</button></div>
+            </form>
+          </div>
+          ) : (null)
+        }
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDe8Wxx-CukPkzRgKRuhBuX57FVxfYXRKY",
+  authDomain: "sappho-b8699.firebaseapp.com",
+  projectId: "sappho-b8699",
+  storageBucket: "sappho-b8699.appspot.com",
+  messagingSenderId: "342299752210",
+  appId: "1:342299752210:web:48bba7a1567e0ee5b865cb",
+  measurementId: "G-NX3VJ0CF7E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +25,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
