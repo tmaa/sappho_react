48219c4e5ec137c536332d38a5fbbf6d4c42afe9
tmaa <thomasaanum@gmail.com>
2022-02-07 23:44:48 -0500

    Delete from firebase and mongodb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import {getAuth, signInAnonymously, signOut, deleteUser} from 'firebase/auth'
+import { signInAnonymously, 
+  signOut, 
+  deleteUser, 
+  signInWithPhoneNumber,
+  RecaptchaVerifier,
+  setPersistence} from 'firebase/auth'
+import {auth} from "./firebase"
 import axios from 'axios';
 
+
 function App() {
-  const [phoneNumber, setPhoneNumber] = useState()
+  const [phone, setPhone] = useState("")
   const [registerSuccess, setRegisterSuccess] = useState(false)
   const [id, setId] = useState()
   const [name, setName] = useState("")
@@ -12,8 +19,36 @@ function App() {
   const [height, setHeight] = useState("")
   const [email, setEmail] = useState("")
 
+  const [expand, setExpand] = useState(false)
+
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+      'size': 'invisible',
+      'callback': (response) => {
+        
+      }
+    }, auth);
+  }
+
+  const requestOTP = (e) => {
+    console.log("submit clicked")
+    e.preventDefault();
+    if(phone.length >= 2){
+      let phoneNumber = "+1" + phone
+      console.log(phoneNumber)
+      generateRecaptcha();
+      let appVerifier = window.recaptchaVerifier;
+      signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+        .then(res => {
+          console.log(res)
+          window.confirmationResult = res
+        }).catch((err) => {
+          console.log(err)
+        })
+    }
+  }
+
   function onAnonClick(){
-    const auth = getAuth();
     signInAnonymously(auth)
       .then((res) => {
         setId(res.user.uid)
@@ -24,42 +59,52 @@ function App() {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode + ": " + errorMessage)
-      });  }
+      });  
+    }
 
-  function onPhoneClick(number){
-    console.log(number)
-  }
 
-  function logoutClick(){
-    const auth = getAuth();
+  function logoutAndDelete(){
     const user = auth.currentUser;
 
-    if(user.isAnonymous){
-      deleteUser(user).then(() => {
-        axios.delete(`http://localhost:3001/api/users/delete/${id}`)
-        .then(res => {
-          console.log(res)
-        })
-        .catch(err => {
+    if(id){
+      //delete from mongodb
+      user.getIdToken(true).then(token => {
+        axios.delete(`http://localhost:3001/api/users/me/delete`, {
+          headers: {
+            'authorization': `Bearer ${token}`
+          }
+        }).then((res) => {
+          setRegisterSuccess(false)
+          console.log("user deleted from mongodb")
+        }).catch((err) => {
           console.log(err)
         })
+      })
+      //delete from firebase
+      deleteUser(user).then(() => {
         setRegisterSuccess(false)
-        console.log("anon user deleted")
+        console.log("user deleted from firebase")
       }).catch((err) => {
         console.log(err)
       })
     }else{
-      signOut(auth).then(() => {
-        setRegisterSuccess(false)
-        console.log("logged out: ", user)
-      }).catch((error) => {
-        console.log("err: ", error)
-      });
+      console.log("no one signed in")
     }
+
+  }
+
+  function logoutWithoutDelete(){
+    const user = auth.currentUser;
+    signOut(auth).then(() => {
+      setRegisterSuccess(false)
+      console.log("logged out: ", user)
+    }).catch((error) => {
+      console.log("err: ", error)
+    });
   }
 
   function handleSubmit(e){
-    var data = {"id": id, "name": name, "age": age, "height": height, "email": email}
+    var data = {"id": id, "name": name, "age": age, "height": height, "email": email, "phone": phone}
     
     axios.post(`http://localhost:3001/api/users/register`, data)
       .then(res => {
@@ -79,20 +124,28 @@ function App() {
   }
 
   function getCurrentUserInfo(){
-    //use to test token/auth protected api
+    const currentUser = auth.currentUser;
+    console.log(currentUser)
+
+    currentUser.getIdToken(true).then(token => {
+      axios.get(`http://localhost:3001/api/users/me`, {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
+      }).then((res) => {
+        console.log(res.data)
+      }).catch((err) => {
+        console.log(err)
+      })
+    })
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button>log in with Google</button>
-        <div>
-          <button onClick={() => onPhoneClick(phoneNumber)}>log in with phone number</button>
-          <input onChange={e => setPhoneNumber(e.target.value)}></input>
-        </div>
         <button onClick={() => onAnonClick()}>anonymous sign in</button>
-        <button onClick={() => logoutClick()}>delete/log out</button>
-        <h1>{phoneNumber}</h1>
+        <button onClick={() => logoutAndDelete()}>log out & delete</button>
+        <button onClick={() => logoutWithoutDelete()}>log out</button>
         {registerSuccess ? (
           <div>
             <form onSubmit={handleSubmit}>
@@ -102,9 +155,11 @@ function App() {
               <input type="text" value={height} onChange={e => setHeight(e.target.value)} placeholder='height'></input>
               <div>
                 <input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder='email'></input>
+                <input type="text" value={phone} onChange={e => setPhone(e.target.value)} placeholder='phone'></input>
               </div>
               <div><button type="submit">complete registration</button></div>
             </form>
+            <div><button onClick={() => getCurrentUserInfo()}>get current user info</button></div>
           </div>
           ) : (null)
         }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDe8Wxx-CukPkzRgKRuhBuX57FVxfYXRKY",
-  authDomain: "sappho-b8699.firebaseapp.com",
-  projectId: "sappho-b8699",
-  storageBucket: "sappho-b8699.appspot.com",
-  messagingSenderId: "342299752210",
-  appId: "1:342299752210:web:48bba7a1567e0ee5b865cb",
-  measurementId: "G-NX3VJ0CF7E"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
