2584d54241b608453d0dbf2c5469f5b875c50c8b
tmaa <thomasaanum@gmail.com>
2022-02-13 01:59:14 -0500

    added filter/search in node

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
   const [id, setId] = useState()
   const [name, setName] = useState("")
   const [dob, setDob] = useState("")
-  const [height, setHeight] = useState("")
   const [email, setEmail] = useState("")
   const [potentialMatches, setPotentialMatches] = useState([])
   const [currentUserName, setCurrentUserName] = useState("a")
   const [minAge, setMinAge] = useState(18)
   const [maxAge, setMaxAge] = useState(99)
   const [maxDistance, setMaxDistance] = useState(25)
-  const [expand, setExpand] = useState(false)
+  const [gender, setGender] = useState('w')
+  const [showMe, setShowMe] = useState('w')
 
   const generateRecaptcha = () => {
     window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
@@ -80,7 +80,7 @@ function App() {
           }
         }).then((res) => {
           setRegisterSuccess(false)
-          console.log("user deleted from mongodb")
+          console.log("user deleted from db")
         }).catch((err) => {
           console.log(err)
         })
@@ -115,10 +115,12 @@ function App() {
           "id": id, 
           "name": name, 
           "dob": dob, 
-          "height": height, 
           "email": email, 
           "phone": phone, 
-          "coordinates": {longitude: position.coords.longitude, latitude: position.coords.latitude}}
+          "coordinates": {longitude: position.coords.longitude, latitude: position.coords.latitude},
+          "gender": gender,
+          "interested_in": showMe
+        }
         
         console.log(data)
         
@@ -166,13 +168,13 @@ function App() {
     const currentUser = auth.currentUser;
 
     currentUser.getIdToken(true).then(token => {
-      axios.get(`http://localhost:3001/api/search/filter/${id}`, {
+      axios.get(`http://localhost:3001/api/search/filter`, {
         headers: {
           'authorization': `Bearer ${token}`
         }
       }).then((res) => {
-        console.log(res.data)
-        setPotentialMatches(res.data)
+        console.log(res.data.users)
+        setPotentialMatches(res.data.users)
         setFilterSuccess(true)
       }).catch((err) => {
         console.log(err)
@@ -188,9 +190,10 @@ function App() {
 
   function updatePreferences(){
     const prefPayload = {
-      minAge: minAge === null || minAge === "" ? 18 : minAge,
-      maxAge: maxAge === null || maxAge === "" ? 99 : maxAge,
-      maxDistance: maxDistance === null || maxDistance === "" ? 25 : maxDistance,
+      minimum_age: minAge,
+      maximum_age: maxAge,
+      maximum_distance: maxDistance,
+      interested_in: showMe
     }
     const currentUser = auth.currentUser;
     console.log(prefPayload)
@@ -201,12 +204,42 @@ function App() {
         }
       }).then((res) => {
         console.log(res.data)
-        setPotentialMatches(res.data)
-        filterPotentialMatches()
       }).catch((err) => {
         console.log(err)
       })
     })
+  }
+
+  const likeClicked = (e) => {
+    const currentUser = auth.currentUser;
+    const likePayload = {
+      user_id: currentUser.uid,
+      target_user_id: e.target.getAttribute("id"),
+      liked: true
+    }
+    currentUser.getIdToken(true).then(token => {
+      axios.post(`http://localhost:3001/api/actions/like`, likePayload, {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
+      }).then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+    })
+
+    console.log(`my id: ${currentUser.uid} other user id: ${e.target.getAttribute("id")}`)
+    const id = e.target.getAttribute("id")
+    setPotentialMatches(potentialMatches.filter(item => item.id !== id))
+  }
+
+  const dislikeClicked = (e) => {
+    const currentUser = auth.currentUser;
+
+    console.log(`my id: ${currentUser.uid} other user id: ${e.target.getAttribute("id")}`)
+    const id = e.target.getAttribute("id")
+    setPotentialMatches(potentialMatches.filter(item => item.id !== id))
   }
 
   return (
@@ -223,28 +256,48 @@ function App() {
               <h2>{id}</h2>
               <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder='name'></input>
               <input type="date" value={dob} onChange={e => setDob(e.target.value)} placeholder='age'></input>
-              <input type="text" value={height} onChange={e => setHeight(e.target.value)} placeholder='height'></input>
               <div>
                 <input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder='email'></input>
                 <input type="text" value={phone} onChange={e => setPhone(e.target.value)} placeholder='phone'></input>
+                <div>
+                  <label>i am a</label>
+                  <select onChange={e => setGender(e.target.value)} value={gender}>
+                    <option value={'m'}>Man</option>
+                    <option value={'w'}>Woman</option>
+                    <option value={'tm'}>Trans Man</option>
+                    <option value={'tw'}>Trans Woman</option>
+                  </select>
+                  <label>show me</label>
+                  <select value={showMe} onChange={e => setShowMe(e.target.value)}>
+                    <option value={'m'}>Men</option>
+                    <option value={'w'}>Women</option>
+                    <option value={'e'}>Everyone</option>
+                  </select>
+                </div>
               </div>
               <div><button type="submit">complete registration</button></div>
             </form>
             <div><button onClick={() => getCurrentUserInfo()}>get current user info</button></div>
             <div>
-              <input placeholder='min age' onChange={e => setMinAge(e.target.value)}></input>
-              <input placeholder='max age' onChange={e => setMaxAge(e.target.value)}></input>
-              <input placeholder='max distance' onChange={e => setMaxDistance(e.target.value)}></input>
+              <input value={minAge} placeholder='min age' onChange={e => setMinAge(e.target.value)}></input>
+              <input value={maxAge} placeholder='max age' onChange={e => setMaxAge(e.target.value)}></input>
+              <input value={maxDistance} placeholder='max distance' onChange={e => setMaxDistance(e.target.value)}></input>
+              <label>show me</label>
+              <select value={showMe} onChange={e => setShowMe(e.target.value)}>
+                <option value={'m'}>Men</option>
+                <option value={'w'}>Women</option>
+                <option value={'e'}>Everyone</option>
+              </select>
               <button onClick={(() => updatePreferences())}>update preferences</button>
             </div>
             <div style={{paddingBottom: "30px"}}><button onClick={() => filterPotentialMatches()}>filter potential matches</button></div>
-            {(filterSuccess && potentialMatches.data) ? (
-              potentialMatches.data.map((match, index) => {
+            {(filterSuccess && potentialMatches) ? (
+              potentialMatches.map((potentialMatch, index) => {
                 return(
                   <div key={index}>
-                    <label>{match.name} {getAge(match.dob.split("T")[0])} </label>
-                    <button>like</button>
-                    <button>dislike</button>
+                    <label>{potentialMatch.name} {potentialMatch.age} </label>
+                    <button id={potentialMatch.id} onClick={likeClicked}>like</button>
+                    <button id={potentialMatch.id} onClick={dislikeClicked}>dislike</button>
                   </div>
                 )
               })
